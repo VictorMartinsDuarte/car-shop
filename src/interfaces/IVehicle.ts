964e7b5a4cb99eee2a33859fcce67daaecb893a7
Victor Martins <victor.martins.duarte@gmail.com>
2022-07-29 17:18:27 -0300

    Chore: Req-02 Adds IVehicle generic interface

---
+++ src/interfaces/IVehicle.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+
+const VehicleZodSchema = z.object({
+  model: z.string()
+    .min(3, { message: 'Model must have at least 3 characters' }),
+  year: z.number()
+    .gte(1900, { message: 'Year must be greater than or equal 1900' })
+    .lte(2022, { message: 'Year must be less than or equal 2022' }),
+  color: z.string()
+    .min(3, { message: 'Color must have at least 3 characters' }),
+  status: z.boolean().optional(),
+  buyValue: z.number(),
+});
+
+export type IVehicle = z.infer<typeof VehicleZodSchema>;
